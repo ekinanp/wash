3d9512f8f91ebcf8dc70e189f98d8d0d109cf181
Enis Inan <enis.inan@puppet.com>
2019-09-27 20:30:06 -0700

    Deleted blah
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- cmd/internal/find/params/core.go
+++
@@ -1,9 +0,0 @@
-// Package params represents `wash find`'s parameters. These are typically
-// set in `wash find`'s main function.
-package params
-
-import "time"
-
-// ReferenceTime is the reference time that's used for `wash find`'s
-// time predicates. Defaults to `wash find`'s start time.
-var ReferenceTime time.Time
